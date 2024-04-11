e7b0020133e5803a034ea06c3ec7355ed7754f6c
Gilberto <gilvaldezarreola@outlook.com>
2024-04-10 18:02:37 -0600

    First_step

---
+++ src/pages/Contactos.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+function Contactos() {
+    return (
+    <div>Contactos
+        <Link href="/About">About</Link>
+    </div>
+    
+    )
+}
+
+export default Contactos
